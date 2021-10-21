6ebcfaf1ff38e2562f80fba71c7cd71193b11622
Ilya Shumilin <shumilin@skbkontur.ru>
2021-10-21 17:52:29 +0500

    Add hercules implementation

--- Vostok.ServiceDiscovery.Telemetry.Hercules/EventReader/HerculesServiceDiscoveryEventReader.cs
+++ Vostok.ServiceDiscovery.Telemetry.Hercules/EventReader/HerculesServiceDiscoveryEventReader.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
+using Vostok.Hercules.Client.Abstractions.Models;
+using Vostok.Hercules.Client.Abstractions.Queries;
 using Vostok.ServiceDiscovery.Telemetry.Event;
 
 namespace Vostok.ServiceDiscovery.Telemetry.Hercules.EventReader
@@ -10,13 +14,21 @@ namespace Vostok.ServiceDiscovery.Telemetry.Hercules.EventReader
     public class HerculesServiceDiscoveryEventReader
     {
         private readonly HerculesServiceDiscoveryEventReaderSettings settings;
+        private StreamCoordinates coordinates = StreamCoordinates.Empty;
 
         public HerculesServiceDiscoveryEventReader([NotNull] HerculesServiceDiscoveryEventReaderSettings settings)
         {
-            this.settings = settings ?? throw new ArgumentNullException(nameof(settings));;
+            this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
         }
 
-        public async Task<ServiceDiscoveryEvent> ReadAsync(TimeSpan timeout, CancellationToken cancellationToken = new CancellationToken()) =>
-            throw new NotImplementedException();
+        public async Task<IEnumerable<ServiceDiscoveryEvent>> ReadAsync(TimeSpan timeout, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var query = new ReadStreamQuery(settings.StreamName) {Coordinates = coordinates};
+            var readResult = await settings.HerculesStreamClient.ReadAsync(query, timeout, cancellationToken);
+            readResult.EnsureSuccess();
+
+            coordinates = readResult.Payload.Next;
+            return readResult.Payload.Events.Select(herculesEvent => HerculesServiceDiscoveryEventFactory.From(herculesEvent)).ToList();
+        }
     }
 }
