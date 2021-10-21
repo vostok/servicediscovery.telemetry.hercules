a5d18c81f7a331af3c48c2ad7f850ed4d04b96cb
Ilya Shumilin <shumilin@skbkontur.ru>
2021-10-21 17:53:35 +0500

    Add some tests

---
+++ Vostok.ServiceDiscovery.Telemetry.Hercules.Tests/ConversionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.ServiceDiscovery.Telemetry.Event;
+
+namespace Vostok.ServiceDiscovery.Telemetry.Hercules.Tests
+{
+    [TestFixture]
+    internal class ConversionTests
+    {
+        [Test]
+        public void Should_correctly_convert_to_hercules_event()
+        {
+            var sdEvent = new ServiceDiscoveryEvent("app", "replica", ServiceDiscoveryEventKind.ReplicaStart, 
+                timestamp: DateTimeOffset.Now, 
+                properties: new Dictionary<string, string> { {"prop1","val1"}, {"prop2", "val2"}});
+
+            var herculesEvent = HerculesServiceDiscoveryEventFactory.To(sdEvent);
+            var parsedSdEvent = HerculesServiceDiscoveryEventFactory.From(herculesEvent);
+
+            sdEvent.Should().BeEquivalentTo(parsedSdEvent);
+        }
+    }
+}
