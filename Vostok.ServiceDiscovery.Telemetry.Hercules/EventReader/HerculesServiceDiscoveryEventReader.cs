0a0ea571753958f3532dda6f63497790d12c5784
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-04 20:03:49 +0500

    Handle failed reading

--- Vostok.ServiceDiscovery.Telemetry.Hercules/EventReader/HerculesServiceDiscoveryEventReader.cs
+++ Vostok.ServiceDiscovery.Telemetry.Hercules/EventReader/HerculesServiceDiscoveryEventReader.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Vostok.Hercules.Client.Abstractions.Models;
 using Vostok.Hercules.Client.Abstractions.Queries;
+using Vostok.Logging.Abstractions;
 using Vostok.ServiceDiscovery.Telemetry.Event;
 
 namespace Vostok.ServiceDiscovery.Telemetry.Hercules.EventReader
@@ -13,20 +14,30 @@ namespace Vostok.ServiceDiscovery.Telemetry.Hercules.EventReader
     [PublicAPI]
     public class HerculesServiceDiscoveryEventReader
     {
-        private readonly HerculesServiceDiscoveryEventReaderSettings settings;
         private StreamCoordinates coordinates;
+        private readonly HerculesServiceDiscoveryEventReaderSettings settings;
+        private readonly ILog log;
 
-        public HerculesServiceDiscoveryEventReader([NotNull] HerculesServiceDiscoveryEventReaderSettings settings)
+        public HerculesServiceDiscoveryEventReader([NotNull] HerculesServiceDiscoveryEventReaderSettings settings, ILog log)
         {
             this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            this.log = (log ?? LogProvider.Get()).ForContext<HerculesServiceDiscoveryEventReader>();
             coordinates = this.settings.Coordinates;
         }
 
+        [ItemCanBeNull]
         public async Task<IList<ServiceDiscoveryEvent>> ReadAsync(TimeSpan timeout, CancellationToken cancellationToken = new CancellationToken())
         {
             var query = new ReadStreamQuery(settings.StreamName) {Coordinates = coordinates};
             var readResult = await settings.HerculesStreamClient.ReadAsync(query, timeout, cancellationToken).ConfigureAwait(false);
-            readResult.EnsureSuccess();
+            if (!readResult.IsSuccessful)
+            {
+                log.Info(
+                    "Reading failed with status: {Status} and error details: {ErrorDetails}.",
+                    readResult.Status,
+                    readResult.ErrorDetails);
+                return null;
+            }
 
             coordinates = readResult.Payload.Next;
             return readResult.Payload.Events.Select(herculesEvent => HerculesServiceDiscoveryEventFactory.From(herculesEvent)).ToList();
